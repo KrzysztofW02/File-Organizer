83464cd84e62b234e2bbb291ff9e24ab1eaccf47
Krzysztof Wasik <Krzysztof.Wasik2002@gmail.com>
2024-07-15 13:58:18 +0200

    Improve file drop functionality and error handling

--- file_drop.py
+++ file_drop.py
@@ -6,7 +6,7 @@ class FileDropLabel(QLabel):
     def __init__(self, parent=None):
         super(FileDropLabel, self).__init__(parent)
         self.setAcceptDrops(True)
-        self.setText('Drag your file to this field:')
+        self.setText('Drag your files to this field:')
         self.setAlignment(Qt.AlignCenter)
         self.setStyleSheet("QLabel { border: 2px dashed #aaa; font-size: 24px; }")
 
@@ -22,11 +22,10 @@ class FileDropLabel(QLabel):
                 if folder_name:
                     if is_file_valid(file_path):
                         if move_file_to_folder(file_path, folder_name):
-                            QMessageBox.information(self, 'File Moved', f'File moved to {folder_name} folder')
+                            QMessageBox.information(self, 'File Moved', f'File {file_path} moved to {folder_name} folder')
                         else:
-                            QMessageBox.warning(self, 'File Exists', 'A file with the same name already exists and was not replaced.')
+                            QMessageBox.warning(self, 'File Exists', f'A file with the name {file_path} already exists and was not replaced.')
                     else:
-                        QMessageBox.warning(self, 'File Damaged', 'The file is damaged and cannot be moved.')
+                        QMessageBox.warning(self, 'File Damaged', f'The file {file_path} is damaged and cannot be moved.')
                 else:
-                    QMessageBox.warning(self, 'File Not Supported', 'Sorry, this file type is not supported.')
-                break
+                    QMessageBox.warning(self, 'File Not Supported', f'Sorry, the file type of {file_path} is not supported.')
--- file_organizer.py
+++ file_organizer.py
@@ -26,16 +26,17 @@ class MainWindow(QMainWindow):
     def open_file_dialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.ReadOnly
-        file_path, _ = QFileDialog.getOpenFileName(self, "Load your file", "", "All Files (*);;", options=options)
-        if file_path:
-            folder_name = get_folder_for_file(file_path)
-            if folder_name:
-                if is_file_valid(file_path):
-                    if move_file_to_folder(file_path, folder_name):
-                        QMessageBox.information(self, 'File Moved', f'File moved to {folder_name} folder')
+        files, _ = QFileDialog.getOpenFileNames(self, "Load your files", "", "All Files (*);;", options=options)
+        for file_path in files:
+            if file_path:
+                folder_name = get_folder_for_file(file_path)
+                if folder_name:
+                    if is_file_valid(file_path):
+                        if move_file_to_folder(file_path, folder_name):
+                            QMessageBox.information(self, 'File Moved', f'File {file_path} moved to {folder_name} folder')
+                        else:
+                            QMessageBox.warning(self, 'File Exists', f'A file with the same name as {file_path} already exists and was not replaced.')
                     else:
-                        QMessageBox.warning(self, 'File Exists', 'A file with the same name already exists and was not replaced.')
+                        QMessageBox.warning(self, 'File Damaged', f'The file {file_path} is damaged and cannot be moved.')
                 else:
-                    QMessageBox.warning(self, 'File Damaged', 'The file is damaged and cannot be moved.')
-            else:
-                QMessageBox.warning(self, 'File Not Supported', 'Sorry, this file type is not supported.')
+                    QMessageBox.warning(self, 'File Not Supported', f'Sorry, the file type of {file_path} is not supported.')
