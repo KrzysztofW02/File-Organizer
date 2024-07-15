db5ed83c8b95c2173b10210afedeaa11100db161
Krzysztof Wasik <Krzysztof.Wasik2002@gmail.com>
2024-07-15 10:21:12 +0200

    Add organizer for images files

--- file_drop.py
+++ file_drop.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from PyQt5.QtWidgets import QLabel, QMessageBox
 from PyQt5.QtCore import Qt
 
@@ -17,5 +19,18 @@ class FileDropLabel(QLabel):
         if event.mimeData().hasUrls():
             for url in event.mimeData().urls():
                 file_path = url.toLocalFile()
-                QMessageBox.information(self, 'File Detected', f'File detected: {file_path}')
+                if self.is_image(file_path):
+                    self.move_to_images_folder(file_path)
+                    QMessageBox.information(self, 'File Detected', f'File moved sucesfully')
+                else:
+                    QMessageBox.warning(self, 'File Not Supported', 'Sorry we cannot help you with that type of file')
                 break
+
+    def is_image(self, file_path):
+        return file_path.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))
+
+    def move_to_images_folder(self, file_path):
+        images_folder = os.path.join(os.getcwd(), 'images')
+        if not os.path.exists(images_folder):
+            os.makedirs(images_folder)
+        shutil.move(file_path, images_folder)
--- file_organizer.py
+++ file_organizer.py
@@ -1,12 +1,13 @@
+import os
+import shutil
 from PyQt5.QtWidgets import QMainWindow, QLabel, QPushButton, QFileDialog, QMessageBox, QVBoxLayout, QWidget
-from PyQt5.QtCore import Qt
 from file_drop import FileDropLabel
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("File Organizezr")
+        self.setWindowTitle("File Organizer")
         self.resize(700, 400)
 
         layout = QVBoxLayout()
@@ -26,7 +27,19 @@ class MainWindow(QMainWindow):
     def open_file_dialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.ReadOnly
-        file_path, _ = QFileDialog.getOpenFileName(self, "Load your file", "", "All Files (*);;Text Files (*.txt)", options=options)
+        file_path, _ = QFileDialog.getOpenFileName(self, "Load your file", "", "All Files (*);;Image Files (*.png *.jpg *.jpeg *.gif *.bmp)", options=options)
         if file_path:
-            QMessageBox.information(self, 'File Detected', f'File detected: {file_path}')
+            if self.is_image(file_path):
+                self.move_to_images_folder(file_path)
+                QMessageBox.information(self, 'File Detected', f'File moved sucesfully')
+            else:
+                QMessageBox.warning(self, 'File Not Supported', 'Sorry we cannot help you with that type of file')
 
+    def is_image(self, file_path):
+        return file_path.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))
+
+    def move_to_images_folder(self, file_path):
+        images_folder = os.path.join(os.getcwd(), 'images')
+        if not os.path.exists(images_folder):
+            os.makedirs(images_folder)
+        shutil.move(file_path, images_folder)
